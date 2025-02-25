f82bc9f3deef415f56a0397f9c5155dc726489c5
Rayyaccc_ <151702591+Ray-Auditore@users.noreply.github.com>
2025-02-25 13:47:27 +0700

    save

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Axioshop180 from './projek/praktek/jwtpath.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Axioshop180/>
+  </StrictMode>,
+)
